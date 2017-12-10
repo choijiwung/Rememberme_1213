f177f7ba81e3a245dc8a345f88c748b9b206f142
choijiwung <784wldnd@naver.com>
2017-12-10 17:42:15 +0900

    package명 정리

---
+++ app/src/main/java/rememberme/io/rememberme/Photo/Photo.java
@@ -0,0 +1,8 @@
+package rememberme.io.rememberme.Photo;
+
+/**
+ * Created by JW on 2017-12-10.
+ */
+
+public class Photo {
+}
---
+++ app/src/main/java/rememberme/io/rememberme/Trip/Trip.java
@@ -0,0 +1,15 @@
+package rememberme.io.rememberme.Trip;
+
+import java.util.Date;
+
+/**
+ * Created by JW on 2017-12-10.
+ */
+
+public class Trip {
+    private String title;
+    private String description;
+    private String region;
+    private Date start;
+    private Date end;
+}
