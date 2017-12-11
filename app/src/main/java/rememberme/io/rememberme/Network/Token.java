faedbf49c6aa00e41d9fe873b1507133a4574ff0
whdqhd5402 <whdqhd5402@gmail.com>
2017-12-11 15:45:51 +0900

    add sharedpreferences with token

--- app/src/main/java/rememberme/io/rememberme/Network/Token.java
+++ app/src/main/java/rememberme/io/rememberme/Network/Token.java
@@ -1,9 +1,24 @@
 package rememberme.io.rememberme.Network;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by JW on 2017-12-10.
  */
 
-public class Token {
-    String key;
+public class Token extends AppCompatActivity {
+
+    public String getKey() {
+        SharedPreferences sharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("key", null);
+    }
+
+    public void setKey(String key) {
+        SharedPreferences sharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("key", key);
+
+    }
 }
--- app/src/main/java/rememberme/io/rememberme/User/LoginActivity.java
+++ app/src/main/java/rememberme/io/rememberme/User/LoginActivity.java
@@ -1,8 +1,6 @@
 package rememberme.io.rememberme.User;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -15,6 +13,7 @@ import android.widget.Toast;
 import rememberme.io.rememberme.Main.MainActivity;
 import rememberme.io.rememberme.Network.APINetwork;
 import rememberme.io.rememberme.Network.ApplicationController;
+import rememberme.io.rememberme.Network.Token;
 import rememberme.io.rememberme.R;
 import rememberme.io.rememberme.User.Results.ULoginResult;
 import retrofit2.Call;
@@ -74,18 +73,8 @@ public class LoginActivity extends AppCompatActivity {
                     ULoginResult ULoginResult = response.body();
                     Log.i("Sign", "Login Success / msg : ".concat(ULoginResult.msg).concat(", token : ".concat(ULoginResult.token)));
 
-                    // 받은 토큰 디비에 저장 저장 - 동우
-                    /* TokenDatabase tokenDatabase = new TokenDatabase();
-                    tokenDatabase.openDatabase();
-                    tokenDatabase.createTable();
-                    tokenDatabase.insertToken(ULoginResult.token.toString().trim());
-                    tokenDatabase.selectToken(ULoginResult.token.toString().trim());*/
-
-                    // DB에 토큰값 받기
-                    /* openDatabase("token.db");
-                    createTable();
-                    insertData(ULoginResult.token.toString().trim());*/
-                    //selectData();
+                    Token token = new Token();
+                    token.setKey(ULoginResult.token);
 
                     Intent loginIntent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(loginIntent);
@@ -101,52 +90,6 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i("Sign", t.getMessage());
             }
         });
-    }
-
-
-    // DB 관리하기 위한 메소드
-    String tableName = "tokenTable";
-    SQLiteDatabase db;
-
-    public void openDatabase(String dbName) {
-        db = openOrCreateDatabase(dbName, MODE_PRIVATE, null);
-        if (db != null) {
-            Log.d("DB", "DB오픈됨");
-        }
-    }
-
-
-    public void createTable() {
-        if (db != null) {
-            String sql = "create table if not exists " + tableName + " (token text) ";
-            db.execSQL(sql);
-            Log.d("DB", "테이블 생성됨");
-        } else {
-            Log.d("DB", "DB를 먼저 오픈하세요");
-        }
-    }
-
-    public void insertData(String token) {
-        if (db != null) {
-            String sql = "insert into " + tableName + "(token) values(" + token + ")";
-            db.execSQL(sql);
-            Log.d("DB", "DB에 추가함");
-        } else {
-            Log.d("DB", "DB 생성이 안됨");
-        }
-    }
-
-    public void selectData() {
-        if (db != null) {
-            String sql = "select token from " + tableName; //" where token = " + token.trim();
-            Cursor selectToken = db.rawQuery(sql, null); // 조회된 토큰이 Cursor 객체로 리턴됨.
-
-            for (int i = 0; i < selectToken.getCount(); i++) {
-                selectToken.moveToNext();
-                String receviedToken = selectToken.getString(0);
-                Log.d("DB", receviedToken);
-            }
-        }
     }
 }
 
